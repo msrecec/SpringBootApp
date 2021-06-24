f65d0553850e69781953b21354aa64b9d5121990
msrecec <67606889+msrecec@users.noreply.github.com>
2021-06-24 23:37:53 +0200

    Starting Setup

---
+++ src/test/java/com/example/vaccines/VaccinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaccines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
